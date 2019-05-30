61090a1c89bdfa7fe55b18984263a760ef00514f
wangsy <wangsy@qq.com>
2019-05-31 07:09:36 +0800

    if else

---
+++ lesson1.py
@@ -0,0 +1,36 @@
+# print('hello python')
+# 流程控制语句
+
+''' mood = False
+a = 1
+b = 2
+c = 3
+
+if not a or b and not c :
+  print('go to left')
+else :
+  print('go to right') '''
+""" 
+account = 'ff'
+password = 123
+print('please input account')
+user_account = input()
+print(' please input password')
+user_password = input()
+
+if account == user_account and password == user_password :
+  print('success')
+else :
+  print('error') """
+""" 
+a = 1
+b = 2
+if a == 1:
+  print('a')
+elif b == 1:
+  print('b') """
+a = 1
+while a:
+  pass
+else:
+  print('dddfdf')
